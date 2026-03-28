c4b1253d62089c074ad29acc98563267e3f0def0
niwa <103595765+arthurniwa@users.noreply.github.com>
2026-03-28 20:20:39 -0300

    Update

--- PocketFinance.Core/Meta.cs
+++ PocketFinance.Core/Meta.cs
@@ -5,29 +5,28 @@ namespace PocketFinance.Core
 {
     public class Meta
     {
-        public int Id {get; set; }
+        public int Id { get; set; }
 
         [Required(ErrorMessage = "De um nome a sua meta")]
+        public string Nome { get; set; } = string.Empty;
 
-        public string Nome {get; set; } = string.Empty;
+        [Required(ErrorMessage = "Quanto você quer juntar?")]
+        public decimal ValorMeta { get; set; }
 
-        [Range(0.01, 99999999, ErrorMessage = "Defina um valor alvo")]
+        public decimal ValorAtual { get; set; }
 
-        public decimal ValorAlvo {get; set; }
+        [Required(ErrorMessage = "Quando você quer realizar?")]
+        public DateTime DataAlvo { get; set; }
 
-        public decimal ValorAtual {get; set;}
-
-        public DateTime DataLimite {get; set; }
-
-        public string UsuarioId {get; set; } = string.Empty;
+        public string UsuarioId { get; set; } = string.Empty;
 
         public int PorcentagemConcluida
         {
-            get
+            get 
             {
-                if (ValorAlvo == 0) return 0;
+                if (ValorMeta == 0) return 0;
 
-                var pct = (int) ((ValorAtual / ValorAlvo) * 100);
+                var pct = (int)((ValorAtual / ValorMeta) * 100);
 
                 return pct > 100 ? 100 : pct;
             }
---
+++ PocketFinance.Core/Migrations/20260217233544_AtualizarMetas.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PocketFinance.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizarMetas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ValorAlvo",
+                table: "Metas",
+                newName: "ValorMeta");
+
+            migrationBuilder.RenameColumn(
+                name: "DataLimite",
+                table: "Metas",
+                newName: "DataAlvo");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ValorMeta",
+                table: "Metas",
+                newName: "ValorAlvo");
+
+            migrationBuilder.RenameColumn(
+                name: "DataAlvo",
+                table: "Metas",
+                newName: "DataLimite");
+        }
+    }
+}
